cf860150ea9b30210cd90cab72e17bc72fb35151
atsuo yamada <yamada-git2@tir.jp>
2017-04-12 19:17:02 +0900

    wip

---
+++ scripts/filter-unmark.clj
@@ -0,0 +1,24 @@
+(require '[leiningen.exec :refer [deps]])
+;(deps '[[foo/bar-core "1.2.3"]])
+(require '[clojure.string :as string])
+(require '[clojure.java.io :as io])
+
+
+(defn usage [args]
+  (binding [*out* *err*]
+    (println "usage : cat input-file | lein exec" (first args) "[mark] > output-file")))
+
+(defn doit [args]
+  (if-let [mark (second args)]
+    (loop []
+      (when-let [line (read-line)]
+        (cond
+          (= "" line) (newline)
+          (re-find #"^#" line) nil
+          :else (when-let [[_ m stripped] (re-find #"^(.) (.*)" line)]
+                  (when-not (= mark m)
+                    (println stripped))))
+        (recur)))
+    (usage args)))
+
+(doit *command-line-args*)
